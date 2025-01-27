8f6620482c473f43895cee3ee2947a11087cd52b
ajdude9 <adamtheteeth@hotmail.com>
2025-01-27 19:38:50 +0000

    Added save slot screens to both scenes and load game menu to the main menu

--- Assets/Scripts/Menu Scripts/MenuController.cs
+++ Assets/Scripts/Menu Scripts/MenuController.cs
@@ -8,10 +8,17 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+
+    private string currentView = "menu";
+    private Camera mainCamera;
+    private Canvas saveLoadCanvas;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCamera = GameObject.Find("Main Camera").GetComponent<Camera>();        
+        saveLoadCanvas = GameObject.Find("SaveSlotCanvas").GetComponent<Canvas>();
+        saveLoadCanvas.enabled = false;
     }
 
     // Update is called once per frame
@@ -38,5 +45,24 @@ public class MenuController : MonoBehaviour
             Application.Quit();//Quit the game and close the window
         #endif
         
+    }
+
+    public void switchViews()
+    {
+        switch(currentView)
+        {
+            case "menu":
+                mainCamera.transform.position = new Vector3(-25, 1, 15);
+                mainCamera.transform.eulerAngles = new Vector3(0, -90, 0);
+                saveLoadCanvas.enabled = true;
+                currentView = "saves";
+            break;
+            case "saves":
+                mainCamera.transform.position = new Vector3(0, 1, -10);
+                mainCamera.transform.eulerAngles = new Vector3(0, 0, 0);
+                saveLoadCanvas.enabled = false;
+                currentView = "menu";
+            break;
+        }
     }
 }
