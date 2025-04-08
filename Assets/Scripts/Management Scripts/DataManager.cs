1dc5bf2097904f3c3e44927cd304d472c634b49c
ajdude9 <adamtheteeth@hotmail.com>
2025-04-08 19:26:48 +0100

    Added various try and catch statements to catch the errors being produced; continued debugging with list wrappers

--- Assets/Scripts/Management Scripts/DataManager.cs
+++ Assets/Scripts/Management Scripts/DataManager.cs
@@ -3,16 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Data.Common;
 using System.IO;
+using System.Numerics;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using Vector3 = UnityEngine.Vector3;
+
 
 public class DataManager : MonoBehaviour
 {
     public static DataManager instance;
-    private CounterController gameController;   
+    private CounterController gameController;
     private BoxMovement box;
 
     private Scene m_Scene;
@@ -20,9 +23,9 @@ public class DataManager : MonoBehaviour
     private int currentProfile;
     private void Awake()
     {
-        
-        if(instance != null)//If Instance already exists; so has already been created
-        {            
+
+        if (instance != null)//If Instance already exists; so has already been created
+        {
             Destroy(gameObject);//Destroy this game object; to prevent duplication.
         }
         else
@@ -30,16 +33,16 @@ public class DataManager : MonoBehaviour
             instance = this;//Set the MainManager script variable, Instance, to this current instance
             DontDestroyOnLoad(gameObject);//Don't destroy this game object when changing scenes            
         }
-        
+
         m_Scene = SceneManager.GetActiveScene();
         sceneName = m_Scene.name;
-        if(sceneName == "Main Game")
+        if (sceneName == "Main Game")
         {
             Debug.Log("Searching for game controller.");
             gameController = GameObject.Find("Game Manager").GetComponent<CounterController>();
             box = GameObject.Find("Box").GetComponent<BoxMovement>();
 
-        }        
+        }
 
     }
 
@@ -64,6 +67,17 @@ public class DataManager : MonoBehaviour
         {
             boolList = newList;
         }
+        public void checkExistence()
+        {
+            try
+            {
+                Debug.Log("Boolean Existence Check Passed");
+            }
+            catch (NullReferenceException e)
+            {
+                Debug.Log("Boolean Existence Error: " + e);
+            }
+        }
     }
 
     [System.Serializable]
@@ -87,6 +101,17 @@ public class DataManager : MonoBehaviour
         {
             vectorList = newList;
         }
+        public void checkExistence()
+        {
+            try
+            {
+                Debug.Log("Vector Existence Check Passed");
+            }
+            catch (NullReferenceException e)
+            {
+                Debug.Log("Vector Existence Error: " + e);
+            }
+        }
     }
 
 
@@ -101,10 +126,11 @@ public class DataManager : MonoBehaviour
         public int coinsWon;//The amount of gems the player can drop        
         public Vector3 boxLocation;//Where the box currently is.
         public Vector3[] boxDestinations;//Where the box is moving to.
-        
+
         public List<BoolListWrapper> projectileBooleans;//An array containing every projectile and its three boolean values
         public List<VectorListWrapper> projectileVectors;//An array containing every projectile and its location and velocity
         public List<string> projectileTypes;//An array containing every projectile and its material type
+        public BoolListWrapper testWrapper;
     }
 
     [System.Serializable]
@@ -124,9 +150,11 @@ public class DataManager : MonoBehaviour
         data.coinsWon = gameController.getCounter("coins", "");
         data.boxLocation = box.getPos();
         data.boxDestinations = box.getDestinations();
-        
+
         GameObject[] allGems = GameObject.FindGameObjectsWithTag("Projectile");
-        
+        Debug.Log("Test Wrapper Test: ");
+        data.testWrapper.checkExistence();
+
         /**
         List<List<Vector3>> tempVector3s = new List<List<Vector3>>();
         List<List<bool>> tempBools = new List<List<bool>>();
@@ -135,31 +163,56 @@ public class DataManager : MonoBehaviour
         List<Vector3> tempVector3s = new List<Vector3>();
         List<bool> tempBools = new List<bool>();
         List<string> tempTypes = new List<string>();
-        int k = 0;
-        foreach(GameObject gems in allGems)
-        {            
-            BallForward gem = gems.GetComponent<BallForward>();               
+         int k = 0;
+        try
+        {
+           
+            data.projectileBooleans = new List<BoolListWrapper>();
+            data.projectileVectors = new List<VectorListWrapper>();
+            foreach (GameObject gems in allGems)
+            {
+                data.projectileBooleans.Add(new BoolListWrapper());
+                data.projectileVectors.Add(new VectorListWrapper());
+                BallForward gem = gems.GetComponent<BallForward>();
+
+
+                tempBools = gem.gatherBooleans();
+                tempVector3s = gem.gatherVectors();
+                tempTypes.Add(gem.getProjType());
+
+                /**
+                data.projectileBooleans.Add(tempBools);
+                data.projectileVectors.Add(tempVector3s);
+                data.projectileTypes.Add(gem.getProjType());
+                */
+
+                
+                data.projectileBooleans[k].add(gem.getSilent());
+                data.projectileBooleans[k].add(gem.getScored());
+                data.projectileBooleans[k].add(gem.getRecreated());
+                data.projectileVectors[k].add(gem.getLocation());
+                data.projectileVectors[k].add(gem.getVelocity());
+                data.projectileTypes.Add(gem.getProjType());
+                k++;
+            }
+        }
+        catch (NullReferenceException e)
+        {
+            Debug.Log("An error has occurred: " + e);                        
+            Debug.Log("Data List Count: " + k);
+            Debug.Log("Existence Check: ");            
+            data.projectileBooleans[k].checkExistence();
+            data.projectileVectors[k].checkExistence();
+            Debug.Log("Data Lists: " + data.projectileBooleans + ", and " + data.projectileVectors);
+            Debug.Log("Data List Information: " + data.projectileBooleans[k] + ", and " + data.projectileVectors[k]);
             
             
-            tempBools = gem.gatherBooleans();
-            tempVector3s = gem.gatherVectors();
-            tempTypes.Add(gem.getProjType());
-            
-            /**
-            data.projectileBooleans.Add(tempBools);
-            data.projectileVectors.Add(tempVector3s);
-            data.projectileTypes.Add(gem.getProjType());
-            */
-            data.projectileBooleans[k].inherit(tempBools);
-            data.projectileVectors[k].inherit(gem.gatherVectors());
-            data.projectileTypes.Add(gem.getProjType());
-            k++;
         }
         //data.projectileBooleans = tempBools;
         //data.projectileVectors = tempVector3s;
         data.projectileTypes = tempTypes;
-        for(int i = 0; i < data.projectileVectors.Count; i++)
-        {       
+        for (int i = 0; i < data.projectileVectors.Count; i++)
+        {
             /**     
             for(int j = 0; j < data.projectileVectors[i].Count; j++)
             {                                        
@@ -194,8 +247,8 @@ public class DataManager : MonoBehaviour
             box.setPos(data.boxLocation);
             box.setDestinations(data.boxDestinations[0], data.boxDestinations[1]);
             //Load projectile data, one at a time
-            
-            for(int i = 0; i < data.projectileBooleans.Count; i++)
+
+            for (int i = 0; i < data.projectileBooleans.Count; i++)
             {
                 /**
                 bool loadedSilent = data.projectileBooleans[i][0];
@@ -207,16 +260,16 @@ public class DataManager : MonoBehaviour
                 string loadedType = data.projectileTypes[i];
                 //gameController.createGem(loadedSilent, loadedScored, loadedRecreated, loadedLocation, loadedVelocity, loadedType);
             }
-            
+
 
         }
     }
 
-    
+
 
     public void quickLoad()
     {
-        string persPath = Application.persistentDataPath + "/persistentData.json";        
+        string persPath = Application.persistentDataPath + "/persistentData.json";
         if (File.Exists(persPath))
         {
             string persJson = File.ReadAllText(persPath);
@@ -232,7 +285,7 @@ public class DataManager : MonoBehaviour
             string savePath = Application.persistentDataPath + "/save_slot_" + i + ".json";//Define the path as the path set in the save data   
             //Debug.Log("Looking for Slot" + i + "GemsText");
             //TextMeshPro gemSlot = GameObject.Find("Slot" + i + "GemsText").GetComponent<TextMeshPro>();
-            TextMeshProUGUI gemSlot = GameObject.Find("Slot" + i + "GemsText").gameObject.GetComponent<TextMeshProUGUI>();            
+            TextMeshProUGUI gemSlot = GameObject.Find("Slot" + i + "GemsText").gameObject.GetComponent<TextMeshProUGUI>();
             TextMeshProUGUI coinsSlot = GameObject.Find("Slot" + i + "CoinsText").GetComponent<TextMeshProUGUI>();
             TextMeshProUGUI bankSlot = GameObject.Find("Slot" + i + "BankText").GetComponent<TextMeshProUGUI>();
             if (File.Exists(savePath))//If we find a file in the filepath
@@ -241,7 +294,7 @@ public class DataManager : MonoBehaviour
                 SaveData data = JsonUtility.FromJson<SaveData>(json);
                 gemSlot.text = "Gems: " + data.gemsSaved[0] + ", " + data.gemsSaved[1] + ", " + data.gemsSaved[2] + ", " + data.gemsSaved[3];
                 coinsSlot.text = "Coins: " + data.coinsWon;
-                bankSlot.text = "Bank: " + data.coinsBanked;                
+                bankSlot.text = "Bank: " + data.coinsBanked;
             }
             else
             {
@@ -251,7 +304,7 @@ public class DataManager : MonoBehaviour
             }
         }
     }
-    
+
     public void setCurrentProfile(int newValue)
     {
         currentProfile = newValue;
@@ -271,8 +324,7 @@ public class DataManager : MonoBehaviour
 
     public void logObjects()
     {
-        
+
     }
-    
+
 }
- 
